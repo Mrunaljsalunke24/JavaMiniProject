4d7f1606f8e0431109c5fe5463e32dad61259338
Mrunal <mrunaljsalunke@gmail.com>
2023-10-23 22:13:32 +0530

    Initial commit

---
+++ src/main/java/org/byteqore/NumberOperations.java
@@ -0,0 +1,23 @@
+package org.byteqore;
+
+public class NumberOperations {
+
+    // create a single or multiple constructors or method input from user to get selection from user to perform operations
+    // on the user given values. here you need to give list of operations specific to numbers and based on that accept the inputs
+    // and perform operations
+
+    //1. check if number is even or odd
+    //2. check if number is prime
+    //3. check if number is pallindrome
+    //4. check if number is negative or positive
+    //5. Print all odd numbers between given range
+    //6. Print all even numbers between given range
+    //7. print product
+    //8. print addition
+    //9. print subtraction
+    //10. print division
+    //11. print factorial
+
+
+
+}
